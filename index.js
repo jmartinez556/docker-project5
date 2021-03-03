ee4bf0bd470ff8060d7752a2c0e8a7b9148e4bba
Your Name <you@example.com>
2021-03-02 20:03:35 -0600

    project5 test

---
+++ index.js
@@ -0,0 +1,13 @@
+//Load express module with `require` directive
+var express = require('express')
+var app = express()
+//Define port
+var port = 9994
+//Define request response in root URL (/)
+app.get('/', function (req, res) {
+    res.send('coding for dummies')
+})
+//Launch listening server on port 9994
+app.listen(port, function () {
+    console.log('app listening on port ${port}!')
+})
